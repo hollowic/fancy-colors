aa22d864073fdf2c9856392b24f4fd6ce936fd37
Jay Zhou <hollowic@hotmail.com>
2020-04-15 15:28:18 -0700

    fixed nested array bug

--- src/Comp/Main/AlternateShades/AlternateShades.jsx
+++ src/Comp/Main/AlternateShades/AlternateShades.jsx
@@ -3,24 +3,20 @@ import "./AlternateShadesStyles.scss";
 import { generateShades } from "../../../Helpers/helperFn";
 
 export default function AlternateShades({ initialValue }) {
-  const [colorShades, setColorShades] = useState([
-    generateShades(initialValue),
-  ]);
+  const [colorShades, setColorShades] = useState(generateShades(initialValue));
 
-  //   useEffect(() => {
-  //     setColorShades(generateShades(initialValue).flatMap((x) => x));
-  //   }, [initialValue]);
+  useEffect(() => {
+    setColorShades(generateShades(initialValue));
+  }, [initialValue]);
 
   return (
     <div className="alternate-container">
       {colorShades.map((el) => {
-        return el.map((el) => {
-          return (
-            <div key={el} className="shades" style={{ background: el }}>
-              <div>{el}</div>
-            </div>
-          );
-        });
+        return (
+          <div key={el} className="shades" style={{ background: el }}>
+            <div>{el}</div>
+          </div>
+        );
       })}
     </div>
   );
