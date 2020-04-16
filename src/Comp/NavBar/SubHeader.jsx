0876180af9b337eb76ed75a5806c0f56b0cb7fd3
Jay Zhou <hollowic@hotmail.com>
2020-04-15 17:14:58 -0700

    Added tooltips to subheader icons

--- src/Comp/NavBar/SubHeader.jsx
+++ src/Comp/NavBar/SubHeader.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import "./SubHeaderStyles.scss";
+import Tooltip from "@material-ui/core/Tooltip";
+
 export default function SubHeader() {
   return (
     <div className="sub-header">
       <p>Press the spacebar to generate color schemes!</p>
       <ul>
         <li>
-          <i className="fas fa-question-circle"></i>
+          <Tooltip title="Help" arrow placement="top">
+            <i className="fas fa-question-circle"></i>
+          </Tooltip>
         </li>
         <li>
-          <i className="fas fa-cog"></i>
+          <Tooltip title="Settings" arrow placement="top">
+            <i className="fas fa-cog"></i>
+          </Tooltip>
         </li>
         <li>
-          <i className="fas fa-camera"></i>
+          <Tooltip title="Pick Colors From Image" arrow placement="top">
+            <i className="fas fa-camera"></i>
+          </Tooltip>
         </li>
         <li>
-          <i className="fas fa-share-alt"></i>
+          <Tooltip title="Toggle Alternative Shades" arrow placement="top">
+            <i className="fas fa-th" />
+          </Tooltip>
         </li>
         <li>
-          <i className="fas fa-bars"></i>
+          <Tooltip title="Export" arrow placement="top">
+            <i className="fas fa-share-alt"></i>
+          </Tooltip>
+        </li>
+        <li>
+          <Tooltip title="Saved Palettes" arrow placement="top">
+            <i className="fas fa-bars"></i>
+          </Tooltip>
         </li>
       </ul>
     </div>
